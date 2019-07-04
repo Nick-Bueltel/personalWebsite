8f4aa89541fbc383dfb8e1d3732036b9e86d0715
NickBueltel <nickbueltel@gmail.com>
2019-07-04 13:06:32 -0500

    layout styling semanticui

--- folio-react/src/App.js
+++ folio-react/src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Item} from 'semantic-ui-react'
 
 function App() {
   return(
-    <h1>TEST</h1>
+    <Item.Group>
+      <Item>
+          <Item.Image size="large" src='https://i.imgur.com/9S4nlmb.png'/>
+          <Item.Content verticalAlign='middle'>
+              <Item.Header as='a' href="https://austinstreetscenes.herokuapp.com" target="blank"> Austin Street Scenes (Django, HTML, CSS, MongoDB) </Item.Header>
+          </Item.Content>
+      </Item>
+      <Item>
+          <Item.Image size="large" src='https://i.imgur.com/Luku1q4.png'/>
+          <Item.Content verticalAlign='middle'>
+              <Item.Header as='a' href="https://thebrewreview.herokuapp.com" target="blank"> The Brew Review (Express, Node, HTML, CSS, PostgreSQL) </Item.Header>
+          </Item.Content>
+      </Item>
+      <Item>
+          <Item.Image size="large" src='https://i.imgur.com/SehZynG.png'/>
+          <Item.Content verticalAlign='middle'>
+              <Item.Header as='a' href="https://nick-bueltel.github.io/puzzleHorrorGame/" target="blank"> Puzzle Horror Game (HTML, CSS, JS) </Item.Header>
+          </Item.Content>
+      </Item>
+    </Item.Group>
+    
   )
 }
 
